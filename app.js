b3bfb43709630244ac570a83ff8611674e447806
Luis Silva <ifaf.silva12@gmail.com>
2022-05-01 23:28:50 +0100

    c

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.get('/api/embarcacao', embarcacao.getEmbarcacoes)
 app.get('/api/embarcacao/:id(\\d+)',embarcacao.getEmbarcacao)
 app.post('/api/embarcacao', embarcacao.addEmbarcacao)
 app.delete('/api/embarcacao/:id(\\d+)',embarcacao.deleteEmbarcacao)
+app.put('/api/embarcacao', embarcacao.updateEmbarcacao)
 
 /*                  EVENTOS              */
 app.get('/api/eventos', eventos.getEventos)
