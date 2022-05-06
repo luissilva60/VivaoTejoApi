a5b9a69af934bdf0925d2a9eaf4fa3956d336d79
Luís Silva <ifaf.silva12@gmail.com>
2022-05-06 11:18:40 +0100

    c

---
+++ public/javascripts/cais.js
@@ -0,0 +1,28 @@
+window.onload = async function() {
+    let cais;
+    let html;
+    var caisGeoJson;
+    const api_url='https://cors-anywhere.herokuapp.com/https://vivaotejo.herokuapp.com/api/cais/1'
+    async function getCais(){
+        const response=await fetch(api_url);
+        const data=await response.json ();
+        //const { geojson } = data;
+        console.log(data);
+        console.log(data.get(0).cais_id);
+
+    }
+    getCais();
+
+}
+
+/*function createCaisHTML(cais) {
+    return "(cais id)= " + cais.cais_id + ";" +
+        "(cais nome)= " + cais.cais_name + ";" +
+        "(cais spot)= " + cais.cais_spot;
+}
+
+function showCais(id) {
+    console.log("setItem->caisId = " + id);
+    sessionStorage.setItem("caisId", id);
+    window.location = "mapateste.html";
+}*/
